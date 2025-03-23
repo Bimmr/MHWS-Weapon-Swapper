27da1e1dd96c8b942ab4b7f4d2497d16ac006a03
Bimmr <Bimmr6696@gmail.com>
2025-03-23 11:56:31 -0400

    Stops current animation to prevent being stuck in a block

--- autorun/WeaponSwapper.lua
+++ autorun/WeaponSwapper.lua
@@ -1,13 +1,12 @@
 local version = "0.0.1"
-local I_AM_A_CHEATER = false
 
 local config = require("WeaponSwapper.Config")
 local bindings = require("WeaponSwapper.Bindings")
 local soundManager, battleMusicManager
 
 local swap_weapon = false
-local config_key = "swapkey"
 
+-- Check if the player is in battle
 local function is_in_battle()
     if soundManager == nil then
         soundManager = sdk.get_managed_singleton("app.SoundMusicManager")
@@ -17,11 +16,8 @@ local function is_in_battle()
         battleMusicManager = soundManager:get_BattleMusic()
     end
     if battleMusicManager == nil then return false end
-    if battleMusicManager:get_IsBattle() then return true end
-    return false
+    return battleMusicManager:get_IsBattle()
 end
-
-
 
 -- Request to swap weapon
 local function request_swap_weapon()
@@ -29,22 +25,25 @@ local function request_swap_weapon()
 end
 
 -- Load the binding from the config
-local binding_config = config.get(config_key)
+local binding_config = config.get("swapkey")
+
+-- Load if you're a cheater (Allows weapon swaps inside of a battle)
+local I_AM_A_CHEATER = config.get("I am a Cheater")
+if I_AM_A_CHEATER == nil then config.set("I am a Cheater", false) end
 
 -- Add the binding to the bindings
 if binding_config ~= nil then
     bindings.add(binding_config.device, binding_config.hotkeys, request_swap_weapon)
 end
 
+-- On REFramework draw UI
 re.on_draw_ui(function()
     if imgui.collapsing_header("Weapon Swapper") then
-
         -- Create the binding listener
         local listen = bindings.listener.create("weaponswapper")
 
         -- On listener complete set the new hotkeys
         listen.on_complete(function()
-            
             -- Remove the old binding
             if binding_config ~= nil then
                 bindings.remove(binding_config.device, binding_config.hotkeys)
@@ -58,26 +57,22 @@ re.on_draw_ui(function()
 
             -- Add the new binding, and save it to the config
             bindings.add(binding_config.device, binding_config.hotkeys, request_swap_weapon)
-            config.set(config_key, binding_config)
+            config.set("swapkey", binding_config)
         end)
-
 
         -- Create the hotkey string
         local hotkey_string = ""
 
         if listen.is_listening() then
-
             -- If listening, and inputs have been started - display the hotkeys being pressed
             if #listen.get_inputs() ~= 0 then
                 for i, input in ipairs(listen.get_inputs()) do
-                    hotkey_string = hotkey_string .. input
-                    hotkey_string = hotkey_string .. " + "
+                    hotkey_string = hotkey_string .. input .. " + "
                 end
             else
                 -- If listening, but no inputs have been started - display listening
                 hotkey_string = "Listening..."
             end
-
         -- If not listening, display the hotkeys from the config
         elseif binding_config == nil or binding_config.hotkeys == nil then
             hotkey_string = "Not Set"
@@ -89,7 +84,7 @@ re.on_draw_ui(function()
                 end
             end
         end
-        
+
         -- Display the hotkey string, and the change hotkey button
         imgui.push_id("WeaponSwapper")
         imgui.indent(2)
@@ -106,15 +101,24 @@ re.on_draw_ui(function()
 
         imgui.spacing()
         imgui.unindent(2)
-
         imgui.pop_id()
     end
 end)
 
--- Keybinds
+-- On REFramework update
 re.on_frame(function()
+    -- Update the bindings
     bindings.update()
 end)
+
+local function stop_action(player)
+    local ActionIDType = sdk.find_type_definition("ace.ACTION_ID")
+    local instance = ValueType.new(ActionIDType)
+    sdk.set_native_field(instance, ActionIDType, "_Category", 1)
+    sdk.set_native_field(instance, ActionIDType, "_Index", 14)
+    player:changeActionRequest(0, instance, false)
+end
+
 
 -- Hook the update function of the HunterCharacter
 -- This will allow us to swap the weapon when the swap_weapon flag is set
@@ -126,10 +130,12 @@ sdk.hook(sdk.find_type_definition("app.HunterCharacter"):get_method("update"), f
     -- If the swap_weapon flag is set, change the weapon
     if swap_weapon then
 
+        -- If the player is not in battle, or the player is a cheater
         if not is_in_battle() or I_AM_A_CHEATER then
+            stop_action(managed)
             managed:changeWeaponFromReserve(true)
         end
+
         swap_weapon = false
     end
-end, function(retval)
-end)
+end, function(retval) end)
