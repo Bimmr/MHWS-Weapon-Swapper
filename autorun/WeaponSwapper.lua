a06446b8a5d18e81bc3fbc132cf72cfe8699973f
Bimmr <Bimmr6696@gmail.com>
2025-03-30 02:36:31 -0400

    Cleaned up code with the use of Github Copilot

--- autorun/WeaponSwapper.lua
+++ autorun/WeaponSwapper.lua
@@ -1,21 +1,40 @@
-local version = "0.0.3"
+local version = "0.0.4"
 
+-- Cached values
+local sdk = sdk
+local imgui = imgui
+local os = os
+
+-- Other required files
 local config = require("WeaponSwapper.Config")
 local bindings = require("WeaponSwapper.Bindings")
 local utils = require("WeaponSwapper.Utils")
 
+-- Cached values
 local action_id_type = sdk.find_type_definition("ace.ACTION_ID")
+local facilitymenu_types = utils.generate_enum("app.FacilityMenu.TYPE")
 
+-- Variables related to swapping weapons
 local swap_weapon = false
-local I_AM_A_CHEATER = false
+local forced_onto_back = false
+local prepare_weapon = false
+local current_weapon
 
-local last_swap_time = 0
 local cooldown = 0.5
+local last_swap_time = 0
+local last_swap_control_time = 0
 
--- Load the binding from the config
-local binding_config = config.get("swapkey")
+-- Variables for tracking the last actions performed by the player
+local last_actions = {}
+local last_action_index = 0
+local max_actions = 5
 
--- Function to stop the current action
+--------------------------------------- Utilities ------------------------------------
+
+-- Create a new ActionID object
+-- @param category: The category of the action (0 = no weapon out, 1 = weapon out, 2 = weapon in action)
+-- @param index: The index of the action (1 = weapon out, 14 =  idle, 15 = moving, 16 = stopping)
+-- @return: A new ActionID object with the specified category and index
 local function get_action_id(category, index)
     local action_id = ValueType.new(action_id_type)
     sdk.set_native_field(action_id, action_id_type, "_Category", category)
@@ -23,59 +42,91 @@ local function get_action_id(category, index)
     return action_id
 end
 
--- Function to check if the player has a facility menu open
+-- Check if the player has a facility menu open
+-- @return: true if a facility menu is open, false otherwise
 local function has_facility_menu_open()
     local gui_manager = sdk.get_managed_singleton("app.GUIManager")
-    local facilitymenu_types = utils.generate_enum("app.FacilityMenu.TYPE")
-
-    local has_facility_menu_open = false
-    for key, value in pairs(facilitymenu_types) do
-        if value > 0 then -- No point in checking INVALID
-            if gui_manager:isActiveMenu(value) then
-                return true
-            end
+    for key, value in ipairs(facilitymenu_types) do
+        if value > 0 and gui_manager:isActiveMenu(value) then
+            return true
         end
     end
     return false
 end
 
--- Request to swap weapon
+-- Request to swap the weapon
+-- Ensure the player is in the game, has weapon handling available, is not in battle (unless a cheater), has no facility menu open, is not in a tent, and the cooldown has elapsed
+-- If all conditions are met, toggle the swap_weapon flag
 local function request_swap_weapon()
-    if not utils.getMasterCharacter() then return end -- If the player is not in the game
-    if not utils.getMasterCharacter():get_WeaponHandling() then return end -- If the player does not have a weapon handling
-    if utils.is_in_battle() and not I_AM_A_CHEATER then return end -- If the player is in battle, and not a cheater
-    if has_facility_menu_open() then return end -- If the player has a facility menu open
-    if utils.getMasterCharacter():get_IsInAllTent() then return end -- If the player is in any tent
-    if os.clock() - last_swap_time < cooldown then return end -- If the player has swapped weapons too quickly
+    if not utils.getMasterCharacter() then return end -- Player is not in the game
+    if not utils.getMasterCharacter():get_WeaponHandling() then return end -- No weapon handling available
+    if utils.is_in_battle() and not I_AM_A_CHEATER then return end -- In battle and not a cheater
+    if has_facility_menu_open() then return end -- Facility menu is open
+    if utils.getMasterCharacter():get_IsInAllTent() then return end -- Player is in a tent
+    if os.clock() - last_swap_time < cooldown then return end -- Cooldown not yet elapsed
 
     swap_weapon = not swap_weapon
 end
 
--- Load if you're a cheater (Allows weapon swaps inside of a battle)
-I_AM_A_CHEATER = config.get("I am a Cheater")
-if I_AM_A_CHEATER == nil then
-    config.set("I am a Cheater", false)
-end
+--------------------------------------- Config ------------------------------------
+local I_AM_A_CHEATER = config.get("I am a Cheater") or false
+config.set("I am a Cheater", I_AM_A_CHEATER)
 
--- Add the binding to the bindings
-if binding_config ~= nil then
-
-    ------------------------------------
-    -- check if first element in hotkeys is a number, if it isn't then we need to convert them
-    -- Remove this check in the future
+local binding_config = config.get("swapkey")
+if binding_config then
+    -- Ensure hotkeys are numeric codes. If not, convert them to numeric codes
     if type(binding_config.hotkeys[1]) ~= "number" then
-        local hotkeys_codes = {}
         local device = binding_config.device
-        for _, hotkey in ipairs(binding_config.hotkeys) do
-            table.insert(hotkeys_codes, bindings.get_code_from_name(device, hotkey))
-        end
-        binding_config.hotkeys = hotkeys_codes
+        binding_config.hotkeys = utils.map(binding_config.hotkeys, function(hotkey)
+            return bindings.get_code_from_name(device, hotkey)
+        end)
         config.set("swapkey", binding_config)
     end
-    ------------------------------------
 
+    -- Add the binding
     bindings.add(binding_config.device, binding_config.hotkeys, request_swap_weapon)
 end
+
+------------------------------------ Action Tracking ------------------------------------
+-- This function is used to keep track of the last 5 actions performed by the player
+-- Only every 3rd action seems to be valid so we track the last 5
+-- @param action_id: The action ID of the action being performed
+local function update_last_actions(action_id)
+    last_action_index = (last_action_index % max_actions) + 1
+    last_actions[last_action_index] = {
+        category = action_id:get_field("_Category"),
+        index = action_id:get_field("_Index"),
+        type = action_id:get_field("_WeaponType")
+    }
+end
+
+-- Check if the given action is being performed within the last 5 tracked action updates
+-- @param category: The category of the action to check, if nil, ignore this check
+-- @param index: The index of the action to check if nil, ignore this check
+-- @return: true if the action is being performed, false otherwise
+local function is_action_current(category, index)
+    for i = 1, max_actions do
+        local action = last_actions[i]
+        if action and (category == nil or action.category == category) and (index == nil or action.index == index) then
+            return true
+        end
+    end
+    return false
+end
+
+-- Check if the weapon is doing something (category is not 0)
+-- @return: true if the weapon is doing something, false otherwise
+local function is_action_weapon_doing_something()
+    for i = 1, max_actions do
+        local action = last_actions[i]
+        if action and action.category ~= 0 then
+            return true
+        end
+    end
+    return false
+end
+
+------------------------------------ REFramework ------------------------------------
 
 -- On REFramework draw UI
 re.on_draw_ui(function()
@@ -157,66 +208,20 @@ re.on_frame(function()
     -- Update the bindings
     bindings.update()
 end)
-
-
-local last_actions = {}
-
--- This function is used to keep track of the last 5 actions performed by the player
--- Only every 3rd action seems to be valid so we track the last 5
-local function update_last_actions(action_id)
-    if #last_actions > 4 then
-        table.remove(last_actions, 1)
-    end
-    local data = {
-        category = action_id:get_field("_Category"), 
-        index = action_id:get_field("_Index"), 
-        type = action_id:get_field("_WeaponType")
-    }
-    table.insert(last_actions, data)
-end
-
--- Check if the given action is being performed within the last 5 tracked action updates
--- @param category: The category of the action to check, if nil, ignore this check
--- @param index: The index of the action to check if nil, ignore this check
--- @return: true if the action is being performed, false otherwise
-local function is_action_current(category, index)
-    local action_found = false
-    for _, action in ipairs(last_actions) do
-        if (category == nil or action.category == category) and (index == nil or action.index == index) then
-            action_found = true
-        end
-    end
-    return action_found
-end
-
--- Check if the weapon is doing something (category is not 0)
--- @return: true if the weapon is doing something, false otherwise
-local function is_action_weapon_doing_something()
-    local action_found = false
-    for _, action in ipairs(last_actions) do
-        if action.category ~= 0 then
-            action_found = true
-        end
-    end
-    return action_found
-
-end
     
 
--- Variables to keep track of weapon_swap
-local forced_onto_back = false
-local last_swap_control_time = 0
-local prepare_weapon = false
-local current_weapon = nil
 -- Update Hook to swap the weapon
 sdk.hook(sdk.find_type_definition("app.HunterCharacter"):get_method("update"), function(args)
-   
     local hunter = sdk.to_managed_object(args[2])
-    if not hunter:get_type_definition():is_a("app.HunterCharacter") then return end
+
+    -- Skip unnecessary logic if conditions are not met
+    if not hunter or not hunter:get_type_definition():is_a("app.HunterCharacter") then return end
     if not hunter:get_IsMaster() then return end
-    if hunter:get_BaseActionController():get_CurrentAction() == nil then return end
-    
-    update_last_actions(hunter:get_BaseActionController():get_CurrentActionID())
+    local base_action_controller = hunter:get_BaseActionController()
+    if not base_action_controller or base_action_controller:get_CurrentAction() == nil then return end
+
+    -- Update the last actions performed by the player
+    update_last_actions(base_action_controller:get_CurrentActionID())
 
     -- If weapon was just swapped and forced onto back, pull it back out
     if not swap_weapon and forced_onto_back and os.clock() - last_swap_control_time > 0.05 then
@@ -241,12 +246,12 @@ sdk.hook(sdk.find_type_definition("app.HunterCharacter"):get_method("update"), f
         end
     end
 
-
     -- If the swap_weapon flag is set, change the weapon
     if swap_weapon then
 
-        -- Check previous action logs to check the weapon (Current action doesn't seem to be constantly updated)
-        local is_weapon_in_hand = hunter:checkWeaponOn() -- Check if the weapon is doing something (category is not 0)
+        -- Check if the weapon is in hand
+        local is_weapon_in_hand = hunter:checkWeaponOn()
+
         if is_weapon_in_hand then
 
             -- Force the weapon onto the back
@@ -256,24 +261,20 @@ sdk.hook(sdk.find_type_definition("app.HunterCharacter"):get_method("update"), f
             forced_onto_back = true
             last_swap_control_time = os.clock()
             return sdk.PreHookResult.CALL_ORIGINAL
-        
 
-        -- Check if the weapon is not out (On back)
         elseif not is_weapon_in_hand and os.clock() - last_swap_control_time > 0.01 then
             
             -- Make sure the weapon swaps
-            if current_weapon == nil then
+            if not current_weapon then
                 current_weapon = hunter:get_Weapon()
             end
 
             -- Swap the weapons
-            hunter:changeWeaponFromReserve(true) -- Swap the weapon, not sure what true/false does here
+            hunter:changeWeaponFromReserve(true)
 
             -- If the weapon is now different
             if hunter:get_Weapon() ~= current_weapon then
                 current_weapon = nil
-            
-                
                 swap_weapon = false
                 last_swap_time = os.clock()
                 last_swap_control_time = os.clock()
@@ -283,9 +284,7 @@ sdk.hook(sdk.find_type_definition("app.HunterCharacter"):get_method("update"), f
                     prepare_weapon = true
                 end
             end
-
         end
     end
-    
 end, function(retval)
 end)
